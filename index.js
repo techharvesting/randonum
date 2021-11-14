6fabdf9fe801153f6e91bee7bd414342e35a8f9d
Naseel Niyas <naseel@techharvesting.in>
2021-11-14 21:17:02 +0530

    Used Node module.exports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Function which returns 1 random number between two numbers
-export function getRandomInt(min, max) {
+module.exports.getRandomInt =  (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
